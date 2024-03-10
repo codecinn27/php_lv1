e2f4e4bbed56cf6cee00b3f0768037e1e51cb72a
Acer <codecinnpro@gmail.com>
2024-03-10 23:58:51 +0800

    prepare

--- prepare.php
+++ prepare.php
@@ -1,3 +1,12 @@
 <?php
     require "conn.php";
+
+    $title = "This is a title";
+    $body = "This is a body";
+
+    //both must work together in order to work together
     $insert = $conn->prepare("INSERT INTO users (title, body) VALUES (:title, :body)");
+    $insert->execute(array(
+        "title" => $title,
+        "body" => $body
+    ));
